c80989e2b72db537638fc2934fb0c73766deff34
jauharmuhammed <jauharmuhammedk@gmail.com>
2022-12-17 18:20:43 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+import datetime
+
+# Create your views here.
+
+def newyear(request):
+    now = datetime.datetime.now()
+    return render(request, 'newyear.html',{
+        'newyear' : now.month == 1 and now.day == 1,
+    })
